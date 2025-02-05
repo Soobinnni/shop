7955333927748f0c57ac2a34d89383c9d48798bd
KIM SOOBIN <xpsxm225@naver.com>
2025-02-05 19:40:32 +0900

    feat: 관심 상품 최저가 업데이트

--- src/main/java/site/soobin/myselectshop/controller/ProductController.java
+++ src/main/java/site/soobin/myselectshop/controller/ProductController.java
@@ -1,10 +1,13 @@
 package site.soobin.myselectshop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.soobin.myselectshop.dto.ProductMypriceRequestDto;
 import site.soobin.myselectshop.dto.ProductRequestDto;
 import site.soobin.myselectshop.dto.ProductResponseDto;
 import site.soobin.myselectshop.naver.service.ProductService;
@@ -18,5 +21,11 @@ public class ProductController {
   @PostMapping("/products")
   public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
     return service.createProduct(requestDto);
+  }
+
+  @PutMapping("/products/{id}")
+  public ProductResponseDto updateProduct(
+      @PathVariable("id") Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+    return service.updateProduct(id, requestDto);
   }
 }
--- src/main/java/site/soobin/myselectshop/naver/service/ProductService.java
+++ src/main/java/site/soobin/myselectshop/naver/service/ProductService.java
@@ -2,6 +2,8 @@ package site.soobin.myselectshop.naver.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.soobin.myselectshop.dto.ProductMypriceRequestDto;
 import site.soobin.myselectshop.dto.ProductRequestDto;
 import site.soobin.myselectshop.dto.ProductResponseDto;
 import site.soobin.myselectshop.entity.Product;
@@ -10,10 +12,33 @@ import site.soobin.myselectshop.repository.ProductRepository;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+  private static final int MIN_MY_PRICE = 100;
   private final ProductRepository repository;
 
   public ProductResponseDto createProduct(ProductRequestDto requestDto) {
     Product product = repository.save(new Product(requestDto));
     return new ProductResponseDto(product);
+  }
+
+  @Transactional
+  public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+    // 가격 검사
+    if (!isOptimalPrice(requestDto.getMyprice())) {
+      throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해주세요.");
+    }
+
+    // 상품 조회
+    Product product =
+        repository.findById(id).orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+
+    // 상품 업데이트
+    product.update(requestDto);
+    repository.save(product);
+
+    return new ProductResponseDto(product);
+  }
+
+  private boolean isOptimalPrice(int price) {
+    return price >= MIN_MY_PRICE;
   }
 }
