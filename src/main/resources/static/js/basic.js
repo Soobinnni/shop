4962eeec1c3aa16c46bb8989911b9bad5fcff68a
KIM SOOBIN <xpsxm225@naver.com>
2025-02-06 05:24:08 +0900

    fix: 최저가 오류 메시지 출력으로 스크립트 수정

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -419,7 +419,10 @@ function setMyprice() {
       window.location.reload();
     },
     error(error, status, request) {
-      logout();
+
+      alert(error.responseJSON?.message || "알 수 없는 오류가 발생했습니다.");
+      window.location.reload();
+      // logout();
     }
   })
 }
